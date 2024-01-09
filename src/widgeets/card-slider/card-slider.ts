b5c30eba5f96fb05c74a259c2901baa2a33a8b19
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2024-01-09 23:22:24 +0300

    Refactor structure

--- src/widgeets/card-slider/card-slider.ts
+++ src/widgeets/card-slider/card-slider.ts
@@ -1,44 +1,44 @@
-import { Swiper } from "swiper";
-import { EffectCreative } from "swiper/modules";
-import { SwiperOptions } from "swiper/types";
+// import { Swiper } from "swiper";
+// import { EffectCreative } from "swiper/modules";
+// import { SwiperOptions } from "swiper/types";
+//
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+//
+// const domSwiper = document.querySelector(".swiper") as HTMLElement;
+// const slidersCount = document.querySelectorAll(".posters-slider .swiper-slide").length;
+//
+// const calcX = () => {
+//   const parentWidth = domSwiper.parentElement?.offsetWidth ?? 0;
+//   const currentWidth = domSwiper.offsetWidth;
+//
+//   let t = (parentWidth - (parentWidth - currentWidth) / 2) / currentWidth;
+//
+//   return (t = Math.max(t, 1)), 100 * t + "%";
+// };
 
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
-const domSwiper = document.querySelector(".swiper") as HTMLElement;
-const slidersCount = document.querySelectorAll(".posters-slider .swiper-slide").length;
-
-const calcX = () => {
-  const parentWidth = domSwiper.parentElement?.offsetWidth ?? 0;
-  const currentWidth = domSwiper.offsetWidth;
-
-  let t = (parentWidth - (parentWidth - currentWidth) / 2) / currentWidth;
-
-  return (t = Math.max(t, 1)), 100 * t + "%";
-};
-
-const swiperOptions: SwiperOptions = {
-  modules: [EffectCreative],
-  effect: "creative",
-  speed: 600,
-  resistanceRatio: 0,
-  grabCursor: true,
-  parallax: true,
-  initialSlide: slidersCount,
-  creativeEffect: {
-    limitProgress: 2,
-    perspective: !0,
-    shadowPerProgress: !0,
-    prev: {
-      shadow: !0,
-      translate: ["-15%", 0, -200],
-    },
-    next: {
-      translate: [calcX(), 0, 0],
-    },
-  },
-};
+// const swiperOptions: SwiperOptions = {
+//   modules: [EffectCreative],
+//   effect: "creative",
+//   speed: 600,
+//   resistanceRatio: 0,
+//   grabCursor: true,
+//   parallax: true,
+//   initialSlide: slidersCount,
+//   creativeEffect: {
+//     limitProgress: 2,
+//     perspective: !0,
+//     shadowPerProgress: !0,
+//     prev: {
+//       shadow: !0,
+//       translate: ["-15%", 0, -200],
+//     },
+//     next: {
+//       translate: [calcX(), 0, 0],
+//     },
+//   },
+// };
 
 // const swiper = new Swiper(domSwiper, swiperOptions);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html,js,pug}"
-  ]
-}
-
+  content: ["./src/**/*.{html,js,pug}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#089DB4",
+          light: "#0DC8DA",
+        },
+      },
+      outlineOffset: {
+        minus2: "-2px",
+      },
+    },
+  },
+};
